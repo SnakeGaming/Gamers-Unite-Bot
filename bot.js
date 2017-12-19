f1b925b58e91c430eae2c7c0db71d649461c4ba7
SnakeGaming <alhussainalmahmodi@gmail.com>
2017-12-18 20:38:39 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require("Discord.js");
+
+const TOKEN = "Mzg3NDA4NDY2NTc2NDA4NTc2.DQjP2g.5Gf7_3CS80-pJgm2vNfSvmi-JFE";
+
+var bot = new Discord.Client();
+
+bot.on("ready",function() {
+        console.log("Ready");
+});
+
+bot.on("message", function(message) {
+        console.log(message.content); 
+        if (message.author.equals(bot.user)) returns;
+        
+                if (message.content == "Thomas") {
+                    message.channel.sendMessage("The booger boi branden")
+                    return 
+                      console.log(message.content)
+                }
+});
+
+bot.login(TOKEN);
